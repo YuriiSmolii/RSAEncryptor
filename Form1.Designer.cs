44bbb317f340f01c809045bdca70c976ffc545f4
Yurii Smolii <45535903+YuriiSmolii@users.noreply.github.com>
2022-05-24 11:02:56 +0300

    Add complete project

---
+++ Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿
+namespace CaesarEncryptor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.OriginalText = new System.Windows.Forms.RichTextBox();
+            this.DecryptedText = new System.Windows.Forms.RichTextBox();
+            this.encryptButton = new System.Windows.Forms.Button();
+            this.decryptButton = new System.Windows.Forms.Button();
+            this.inputBoxLable = new System.Windows.Forms.Label();
+            this.resultBoxLable = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OriginalText
+            // 
+            this.OriginalText.Location = new System.Drawing.Point(12, 42);
+            this.OriginalText.Name = "OriginalText";
+            this.OriginalText.Size = new System.Drawing.Size(342, 213);
+            this.OriginalText.TabIndex = 0;
+            this.OriginalText.Text = "";
+            // 
+            // DecryptedText
+            // 
+            this.DecryptedText.Location = new System.Drawing.Point(446, 42);
+            this.DecryptedText.Name = "DecryptedText";
+            this.DecryptedText.ReadOnly = true;
+            this.DecryptedText.Size = new System.Drawing.Size(342, 213);
+            this.DecryptedText.TabIndex = 1;
+            this.DecryptedText.Text = "";
+            // 
+            // encryptButton
+            // 
+            this.encryptButton.Location = new System.Drawing.Point(360, 120);
+            this.encryptButton.Name = "encryptButton";
+            this.encryptButton.Size = new System.Drawing.Size(75, 23);
+            this.encryptButton.TabIndex = 4;
+            this.encryptButton.Text = "Encrypt";
+            this.encryptButton.UseVisualStyleBackColor = true;
+            this.encryptButton.Click += new System.EventHandler(this.encryptButton_Click);
+            // 
+            // decryptButton
+            // 
+            this.decryptButton.Location = new System.Drawing.Point(360, 149);
+            this.decryptButton.Name = "decryptButton";
+            this.decryptButton.Size = new System.Drawing.Size(75, 23);
+            this.decryptButton.TabIndex = 5;
+            this.decryptButton.Text = "Decrypt";
+            this.decryptButton.UseVisualStyleBackColor = true;
+            this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
+            // 
+            // inputBoxLable
+            // 
+            this.inputBoxLable.AutoSize = true;
+            this.inputBoxLable.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.inputBoxLable.Location = new System.Drawing.Point(12, 24);
+            this.inputBoxLable.Name = "inputBoxLable";
+            this.inputBoxLable.Size = new System.Drawing.Size(146, 15);
+            this.inputBoxLable.TabIndex = 10;
+            this.inputBoxLable.Text = "Opened file / Input text :";
+            // 
+            // resultBoxLable
+            // 
+            this.resultBoxLable.AutoSize = true;
+            this.resultBoxLable.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.resultBoxLable.Location = new System.Drawing.Point(446, 24);
+            this.resultBoxLable.Name = "resultBoxLable";
+            this.resultBoxLable.Size = new System.Drawing.Size(104, 15);
+            this.resultBoxLable.TabIndex = 11;
+            this.resultBoxLable.Text = "Operation result :";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.printToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.saveAsToolStripMenuItem.Text = "Save As";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(114, 22);
+            this.printToolStripMenuItem.Text = "Print";
+            this.printToolStripMenuItem.Click += new System.EventHandler(this.printToolStripMenuItem_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(52, 20);
+            this.toolStripMenuItem1.Text = "About";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem2.Text = "Program";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(446, 273);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 15);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "RSA Private key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 15);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "RSA Public key";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Generate keys";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(446, 291);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(342, 147);
+            this.richTextBox1.TabIndex = 16;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 291);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(342, 147);
+            this.richTextBox2.TabIndex = 15;
+            this.richTextBox2.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 325);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Read keys";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.resultBoxLable);
+            this.Controls.Add(this.inputBoxLable);
+            this.Controls.Add(this.decryptButton);
+            this.Controls.Add(this.encryptButton);
+            this.Controls.Add(this.DecryptedText);
+            this.Controls.Add(this.OriginalText);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RSA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox OriginalText;
+        private System.Windows.Forms.RichTextBox DecryptedText;
+        private System.Windows.Forms.Button encryptButton;
+        private System.Windows.Forms.Button decryptButton;
+        private System.Windows.Forms.Label inputBoxLable;
+        private System.Windows.Forms.Label resultBoxLable;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Xml.Serialization;
+
+namespace CaesarEncryptor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int length;
+        public bool RichTextBoxChanged = false;
+        public bool IsSaved = false;
+        public string FileName = String.Empty;
+        public DataTable frequenceTableOriginal = new DataTable();
+        public DataTable frequenceTableDecrypted = new DataTable();
+        public DataTable frequenceTableUkr = new DataTable();
+        public DataTable frequenceTableEng = new DataTable();
+        public DataTable attackTable = new DataTable();
+        public string public_key;
+        public string private_key;
+        public string RSAkey;
+
+        RSACryptoServiceProvider rSA = new RSACryptoServiceProvider();
+        public byte[] encryptedRSA;
+
+        private void ClearRichTextBox()
+        {
+            OriginalText.Text = "";
+            DecryptedText.Text = "";
+        }
+
+        private void Open(string OpenFileName)
+        {
+
+            if (OpenFileName == "")
+            {
+                return;
+            }
+            else
+            {
+                StreamReader readFile = new StreamReader(OpenFileName);
+                while (!readFile.EndOfStream)
+                {
+                    OriginalText.Text = readFile.ReadToEnd();
+                }
+                readFile.Close();
+                FileName = OpenFileName;
+            }
+        }
+
+        private void Save(string SaveFileName)
+        {
+            if (SaveFileName == "")
+            {
+                return;
+            }
+            else
+            {
+                File.WriteAllText(SaveFileName, String.Empty);
+                StreamWriter sw = new StreamWriter(SaveFileName);
+                sw.WriteLine("Original text ->\n");
+                for (int i = 0; i < OriginalText.Lines.Length; i++)
+                {
+                    sw.WriteLine(OriginalText.Lines[i]);
+                }
+                sw.WriteLine("\n Decrypted text ->\n");
+                for (int i = 0; i < DecryptedText.Lines.Length; i++)
+                {
+                    sw.WriteLine(DecryptedText.Lines[i]);
+                }
+                sw.Flush();
+                sw.Close();
+                FileName = SaveFileName;
+            }
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            About form = new About();
+            form.Show();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (RichTextBoxChanged == true)
+            {
+                DialogResult dialogResult = MessageBox.Show("Save current content?", "Question", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    saveToolStripMenuItem.PerformClick();
+                    this.Text = "RSA";
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    ClearRichTextBox();
+                    this.Text = "RSA";
+                }
+            }
+            else
+            {
+                ClearRichTextBox();
+                this.Text = "RSA";
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (RichTextBoxChanged == true)
+            {
+                if (DecryptedText.Text != String.Empty)
+                {
+                    DialogResult dialogResult = MessageBox.Show("Save current content?", "", MessageBoxButtons.YesNo);
+                    if (dialogResult == DialogResult.Yes)
+                    {
+                        saveToolStripMenuItem.PerformClick();
+                    }
+                    else if (dialogResult == DialogResult.No)
+                    {
+                        DialogResult res;
+                        res = openFileDialog1.ShowDialog();
+                        if (res == DialogResult.OK)
+                        {
+                            Open(openFileDialog1.FileName);
+                            RichTextBoxChanged = false;
+                        }
+                        FileName = openFileDialog1.FileName;
+                        this.Text = Path.GetFileNameWithoutExtension(FileName);
+                    }
+                }
+                else
+                {
+                    DialogResult res;
+                    res = openFileDialog1.ShowDialog();
+                    if (res == DialogResult.OK)
+                    {
+                        Open(openFileDialog1.FileName);
+                        RichTextBoxChanged = false;
+                    }
+                    FileName = openFileDialog1.FileName;
+                    this.Text = Path.GetFileNameWithoutExtension(FileName);
+                }
+            }
+            else
+            {
+                DialogResult res;
+                res = openFileDialog1.ShowDialog();
+                if (res == DialogResult.OK)
+                {
+                    Open(openFileDialog1.FileName);
+                }
+                FileName = openFileDialog1.FileName;
+                this.Text = Path.GetFileNameWithoutExtension(FileName);
+            }
+            saveToolStripMenuItem.Enabled = true;
+            IsSaved = true;
+            RichTextBoxChanged = false;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(FileName))
+            {
+                saveAsToolStripMenuItem.PerformClick();
+            }
+            else
+            {
+                Save(FileName);
+                RichTextBoxChanged = false;
+                IsSaved = true;
+                saveToolStripMenuItem.Enabled = true;
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult res;
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|Word Doucment (*.doc)|*.doc;*.docx|All files (*.*)|*.*";
+            res = saveFileDialog1.ShowDialog();
+            if (res == DialogResult.OK)
+            {
+                Save(saveFileDialog1.FileName);
+                RichTextBoxChanged = false;
+                IsSaved = true;
+            }
+            FileName = saveFileDialog1.FileName;
+            saveToolStripMenuItem.Enabled = true;
+            this.Text = Path.GetFileNameWithoutExtension(FileName);
+        }
+
+        private void encryptButton_Click(object sender, EventArgs e)
+        {
+            string text = OriginalText.Text;
+            if (string.IsNullOrEmpty(private_key) && string.IsNullOrEmpty(public_key))
+            {
+                MessageBox.Show("Firstly generate RSA key pair");
+            }
+            else
+            {
+                byte[] toEncryptData = Encoding.UTF8.GetBytes(text);
+                encryptedRSA = rSA.Encrypt(toEncryptData, false);
+                DecryptedText.Text = Encoding.Default.GetString(encryptedRSA);
+            }
+        }
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            bool valid = true;
+            if (string.IsNullOrEmpty(private_key) && string.IsNullOrEmpty(public_key))
+            {
+                MessageBox.Show("Firstly generate RSA key pair");
+            }
+            else
+            {
+                try
+                {
+                    rSA.FromXmlString(private_key);
+                }
+                catch
+                {
+                    MessageBox.Show("Invalid key pair");
+                    valid = false;
+                }
+                
+                if(valid)
+                {
+                    byte[] decryptedRSA = rSA.Decrypt(encryptedRSA, false);
+                    DecryptedText.Text = Encoding.Default.GetString(decryptedRSA);
+                }
+            }
+        }
+
+        private void printToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            printDialog1.Document = printDocument1;
+            if (printDialog1.ShowDialog() == DialogResult.OK)
+            {
+                printDocument1.Print();
+            }
+            else
+            {
+                MessageBox.Show("Print Cancelled");
+            }
+        }
+
+        private void printDocument1_PrintPage_1(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Font myFont = new Font("Arial", 14, FontStyle.Regular, GraphicsUnit.Pixel);
+            float leftMargin = e.MarginBounds.Left;
+            if (!string.IsNullOrEmpty(DecryptedText.Text))
+            {
+                e.Graphics.DrawString("Original text:\n" + OriginalText.Text + "\n\nDecrytped text:\n" + DecryptedText.Text + "\n\nRSA pub key:\n" + public_key + "\n\nRSA private key:\n" + private_key, myFont, Brushes.Black, leftMargin, 150, new StringFormat());
+            }
+            else if (!string.IsNullOrEmpty(OriginalText.Text))
+            {
+                e.Graphics.DrawString("Original text:\n" + OriginalText.Text + "\n\nRSA pub key:\n" + public_key + "\n\nRSA private key:\n" + private_key, myFont, Brushes.Black, leftMargin, 150, new StringFormat());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            public_key = rSA.ToXmlString(false);
+            private_key = rSA.ToXmlString(true);
+
+            richTextBox2.Text = public_key;
+            richTextBox1.Text = private_key;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            public_key = richTextBox2.Text;
+            private_key = richTextBox1.Text;
+        }
+    }
+}
